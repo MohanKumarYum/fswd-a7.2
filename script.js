0339a4b84eac7349434676993d7ed4460e541aa9
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-30 19:39:50 +1100

    Assignment 2 Completedm, + Output Included.

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Using for loop");
+
+for (let index = 5; index <= 50; index = index + 5) {
+    console.log(index);
+}
+
+console.log("Using while loop");
+
+var index = 5;
+
+while (index <= 50) {
+    console.log(index);
+    index = index + 5;
+}
